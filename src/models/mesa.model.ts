221a0b916992909abdff6d09c36a3d9489c1a0a7
111647CejasFederico <111647@tecnicatura.frc.utn.edu.ar>
2023-09-25 22:57:36 -0300

    KAN-47

---
+++ src/models/mesa.model.ts
@@ -0,0 +1,38 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/mysql.config";
+import { MesaInterface } from "../interfaces/mesa.interface";
+
+class Mesa extends Model<MesaInterface> implements Mesa {
+  id!: number;
+  ubicacion!: number;
+  habilitado!: boolean;
+}
+
+Mesa.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    ubicacion: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    habilitado: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Mesa",
+    tableName: "mesas",
+    timestamps: true,
+    createdAt: "fecha_creacion",
+    updatedAt: "fecha_actualizacion",
+    omitNull: true,
+  }
+);
+
+export default Mesa;
